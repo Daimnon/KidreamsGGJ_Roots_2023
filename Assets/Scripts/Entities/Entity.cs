b66d72f51f538f692ab3cb3d0c90555634846062
Jonathan Levinn <jon.levin25@gmail.com>
2023-02-02 23:56:50 +0200

    wip entity

--- Assets/Scripts/Entities/Entity.cs
+++ Assets/Scripts/Entities/Entity.cs
@@ -1,7 +1,18 @@
 using UnityEngine;
 
-[RequireComponent(typeof(EntityNavigation))]
+[RequireComponent(typeof(EntityNavigation)), RequireComponent(typeof(EntityData))]
 public class Entity : MonoBehaviour
 {
+    private EntityNavigation.NavigationMode _startNavMode;
+    private EntityNavigation _navigation;
+    private EntityData _data;
+
+    private void Awake()
+    {
+        _data = GetComponent<EntityDataHolder>().Data;
+        _navigation = GetComponent<EntityNavigation>();
+        _navigation.NavMode = _startNavMode;
+    }
+    
     
 }
