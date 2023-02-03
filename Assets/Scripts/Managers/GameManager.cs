60b1a598e878fb5c48520572843662f90b9b3fae
Daimnon <49341329+Daimnon@users.noreply.github.com>
2023-02-03 17:39:59 +0200

    Camera manager

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum GameStates { PlayerLoop, VampireLordLoop }
+
 public class GameManager : MonoBehaviour
 {
     private static GameManager _instance;
@@ -9,6 +11,23 @@ public class GameManager : MonoBehaviour
 
     private delegate void GameState();
     private GameState _gameState;
+
+    [SerializeField] private PlayerData _newPlayerData, _nextPlayerData;
+    [SerializeField] private GameObject _playerPrefab, _currentPlayer;
+    [SerializeField] private PlayerController _playerController;
+    [SerializeField] private Transform _playerSpawn, _vampireLordSpawn;
+
+    public PlayerData NewPlayerData => _newPlayerData;
+    public PlayerData NextPlayerData { get => _nextPlayerData; set => value = _nextPlayerData; }
+
+    public GameObject PlayerPrefab => _playerPrefab;
+    public GameObject CurrentPlayer { get => _currentPlayer; set => value = _currentPlayer; }
+
+    public PlayerController PlayerController { get => _playerController; set => value = _playerController; }
+
+    public Transform PlayerSpawn { get => _playerSpawn; set => value = _playerSpawn; }
+    public Transform VampireLordSpawn => _vampireLordSpawn;
+
 
     private void Awake()
     {
@@ -22,10 +41,27 @@ public class GameManager : MonoBehaviour
 
     private void PlayerLoop()
     {
-
+        Debug.Log($"GameState is PlayerLoop");
     }
     private void VampireLordLoop()
     {
+        Debug.Log($"GameState is VampireLordLoop");
+        GameObject newPlayer = Instantiate(_playerPrefab, _playerSpawn);
+        PlayerController newPlayerController = newPlayer.GetComponent<PlayerController>();
+        newPlayerController.Data = _nextPlayerData;
+        ChangeState(GameStates.PlayerLoop);
+    }
 
+    public void ChangeState(GameStates newState)
+    {
+        switch (newState)
+        {
+            case GameStates.PlayerLoop:
+                _gameState = PlayerLoop;
+                break;
+            case GameStates.VampireLordLoop:
+                _gameState = VampireLordLoop;
+                break;
+        }
     }
 }
