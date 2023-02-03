80505d80af71eec71dedc71f9f1abaf2590e42ca
Daimnon <49341329+Daimnon@users.noreply.github.com>
2023-02-03 18:23:25 +0200

    Player don't move?

--- Assets/Scripts/Entities/SpriteDirection.cs
+++ Assets/Scripts/Entities/SpriteDirection.cs
@@ -22,7 +22,7 @@ public class SpriteDirection : MonoBehaviour
     // [SerializeField] private SpriteRenderer spriteRenderer;
     [SerializeField] private Transform graphicsParent;
     [SerializeField] private FaceDirection spriteDefaultDirection = FaceDirection.Right;
-    
+
     public FaceDirection Direction { get; private set; } // In case we need it
 
     public Vector2 Vector => Direction == FaceDirection.Right ? Vector2.right : Vector2.left;
@@ -30,7 +30,7 @@ public class SpriteDirection : MonoBehaviour
     // Naughty
     private bool ModeIsRigidBody() => mode == Mode.RigidBody;
     private bool ModeIsNavMesh() => mode == Mode.NavMesh;
-
+    private bool _wasInit = false;
     private void OnValidate()
     {
         // if (spriteRenderer == null) spriteRenderer = GetComponentInChildren<SpriteRenderer>();
@@ -42,12 +42,15 @@ public class SpriteDirection : MonoBehaviour
     {
         var oldDir = Direction;
         Direction = GetDirection(oldDir);
-        if (Direction != oldDir)
+
+        if (!_wasInit || Direction != oldDir)
         {
             var shouldFlipX = Direction != spriteDefaultDirection;
             // spriteRenderer.flipX = shouldFlipX;
-            graphicsParent.SetLocalScaleX(shouldFlipX ? 1f : -1f);
+            graphicsParent.SetLocalScaleX(shouldFlipX ? -1f : 1f);
         }
+
+        _wasInit = true;
     }
 
     private FaceDirection GetDirection(FaceDirection oldDirection)
