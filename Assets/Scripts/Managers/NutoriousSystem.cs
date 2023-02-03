8ecefcc28063ce4b7d12b0be00bd700bc635951f
Gal Azulay <galazu@gmail.com>
2023-02-03 22:49:54 +0100

    fence

--- Assets/Scripts/Managers/NutoriousSystem.cs
+++ Assets/Scripts/Managers/NutoriousSystem.cs
@@ -1,24 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class NutoriousSystem : MonoBehaviour
 {
     [SerializeField]
-    private int nutoriousPoints = 0;
+    private  int nutoriousPoints = 0;
+    [SerializeField]
+    private int[] spawnMilestones;
+    private int curSpawnMilestone;
     [SerializeField]
     private Entity entityToSpawn;
     [SerializeField]
     private MapManager mapManager;
-
-
-    private void AddPoints() => nutoriousPoints++;
-    private void OnAddPoints()
+    private void Start()
     {
-        if(nutoriousPoints == 3)
-        {
-            var tempObj = Instantiate(entityToSpawn.GetComponent<GameObject>(), mapManager.GetRandomVillagerSpawnPosition(), Quaternion.identity);
-        }
+        CheckSpawnMilestone();
+        curSpawnMilestone == spawnMilestones[0];
+    }
+    private void OnEnable()
+    {
+        Entity.OnEntityDeath += OnEntityDeath;
     }
 
+    private void OnDisable()
+    {
+        Entity.OnEntityDeath -= OnEntityDeath;
+    }
+    private void OnEntityDeath(Entity obj)
+    {
+        if (nutoriousPoints % 3 == 0)
+        {
+            var tempObj = Instantiate(entityToSpawn, mapManager.GetRandomVillagerSpawnPosition(), Quaternion.identity);
+        }
+        nutoriousPoints++;
+
+    }
+    private void OnEntityDeath()
+    {
+        if (nutoriousPoints % 3 == 0)
+        {
+        }
+        nutoriousPoints++;
+
+    }
+    private void CheckSpawnMilestone()
+    {
+        var shouldSpawn = spawnMilestones.Where(milestone => milestone == curSpawnMilestone).Any();
+        if(shouldSpawn)
+        {
+            var tempObj = Instantiate(entityToSpawn, mapManager.GetRandomVillagerSpawnPosition(), Quaternion.identity);
+        }
+    }
 }
